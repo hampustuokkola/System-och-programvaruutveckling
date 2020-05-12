f2adc0b72555f7bd21c24d504957fd4cec50fb3b
timsah171 <timsandelius@live.com>
2020-05-12 11:56:25 +0200

    test

---
+++ C#example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace C_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worrld!");
+            Qwe.SayHi();
+        }
+    }
+    class Qwe {
+        public static void SayHi(){
+            Console.Write("hi\n");
+        }
+    }
+}
+
